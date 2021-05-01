41b83bd9f0c17a654c52e2c8d7687696b2a99188
Cedric Laruelle <cedric@melfatum.com>
2021-05-01 07:32:10 +0200

    fix: bad key or secret tests

--- src/test/java/io/stream/AppTest.java
+++ src/test/java/io/stream/AppTest.java
@@ -1,10 +1,12 @@
 package io.stream;
 
-import io.stream.exceptions.StreamException;
-import io.stream.models.App;
+import java.lang.reflect.Field;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import io.stream.exceptions.StreamException;
+import io.stream.models.App;
+import io.stream.services.framework.StreamServiceGenerator;
 
 public class AppTest extends BasicTest {
 
@@ -25,18 +27,27 @@ public class AppTest extends BasicTest {
 
   @DisplayName("App Get fails with bad key")
   @Test
-  void givenBadKey_whenGettingApp_thenException() {
-    System.setProperty("STREAM_KEY", "XXX");
-    StreamException exception = Assertions.assertThrows(StreamException.class, () -> App.get());
+  void givenBadKey_whenGettingApp_thenException()
+      throws NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
+    Field apiKeyField = StreamServiceGenerator.class.getDeclaredField("apiKey");
+    apiKeyField.setAccessible(true);
+    apiKeyField.set(StreamServiceGenerator.class, "XXX");
+    StreamException exception =
+        Assertions.assertThrows(StreamException.class, () -> App.get().request());
     Assertions.assertEquals(401, exception.getResponseData().getStatusCode());
   }
 
   @DisplayName("App Get fails with bad secret (after enabling auth)")
   @Test
-  void givenBadSecret_whenEnableAuthAndGettingApp_thenException() {
+  void givenBadSecret_whenEnableAuthAndGettingApp_thenException()
+      throws NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
     Assertions.assertDoesNotThrow(() -> App.update().withDisableAuth(false).request());
-    System.setProperty("STREAM_SECRET", "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
-    StreamException exception = Assertions.assertThrows(StreamException.class, () -> App.get());
+    Field apiSecretField = StreamServiceGenerator.class.getDeclaredField("apiSecret");
+    apiSecretField.setAccessible(true);
+    apiSecretField.set(
+        StreamServiceGenerator.class, "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+    StreamException exception =
+        Assertions.assertThrows(StreamException.class, () -> App.get().request());
     Assertions.assertEquals(401, exception.getResponseData().getStatusCode());
   }
 }
