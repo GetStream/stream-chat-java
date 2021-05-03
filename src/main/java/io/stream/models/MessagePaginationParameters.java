6f209ed0a4268d788b1065f042cc8037a3ac6c48
Cedric Laruelle <cedric@melfatum.com>
2021-05-03 13:15:20 +0200

    refactor: use Lombok builders

--- src/main/java/io/stream/models/MessagePaginationParameters.java
+++ src/main/java/io/stream/models/MessagePaginationParameters.java
@@ -1,10 +1,11 @@
 package io.stream.models;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.Date;
-import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
 
+@Builder
 public class MessagePaginationParameters {
   @Nullable
   @JsonProperty("limit")
@@ -45,107 +46,4 @@ public class MessagePaginationParameters {
   @Nullable
   @JsonProperty("created_at_before")
   private Date createdAtBefore;
-
-  private MessagePaginationParameters(Builder builder) {
-    this.limit = builder.limit;
-    this.offset = builder.offset;
-    this.idGte = builder.idGte;
-    this.idGt = builder.idGt;
-    this.idLte = builder.idLte;
-    this.idLt = builder.idLt;
-    this.createdAtAfterOrEqual = builder.createdAtAfterOrEqual;
-    this.createdAtAfter = builder.createdAtAfter;
-    this.createdAtBeforeOrEqual = builder.createdAtBeforeOrEqual;
-    this.createdAtBefore = builder.createdAtBefore;
-  }
-
-  /**
-   * Creates builder to build {@link MessagePaginationParameters}.
-   *
-   * @return created builder
-   */
-  public static Builder builder() {
-    return new Builder();
-  }
-
-  /** Builder to build {@link MessagePaginationParameters}. */
-  public static final class Builder {
-    private Integer limit;
-    private Integer offset;
-    private String idGte;
-    private String idGt;
-    private String idLte;
-    private String idLt;
-    private Date createdAtAfterOrEqual;
-    private Date createdAtAfter;
-    private Date createdAtBeforeOrEqual;
-    private Date createdAtBefore;
-
-    private Builder() {}
-
-    @NotNull
-    public Builder limit(@NotNull Integer limit) {
-      this.limit = limit;
-      return this;
-    }
-
-    @NotNull
-    public Builder offset(@NotNull Integer offset) {
-      this.offset = offset;
-      return this;
-    }
-
-    @NotNull
-    public Builder idGte(@NotNull String idGte) {
-      this.idGte = idGte;
-      return this;
-    }
-
-    @NotNull
-    public Builder idGt(@NotNull String idGt) {
-      this.idGt = idGt;
-      return this;
-    }
-
-    @NotNull
-    public Builder idLte(@NotNull String idLte) {
-      this.idLte = idLte;
-      return this;
-    }
-
-    @NotNull
-    public Builder idLt(@NotNull String idLt) {
-      this.idLt = idLt;
-      return this;
-    }
-
-    @NotNull
-    public Builder createdAtAfterOrEqual(@NotNull Date createdAtAfterOrEqual) {
-      this.createdAtAfterOrEqual = createdAtAfterOrEqual;
-      return this;
-    }
-
-    @NotNull
-    public Builder createdAtAfter(@NotNull Date createdAtAfter) {
-      this.createdAtAfter = createdAtAfter;
-      return this;
-    }
-
-    @NotNull
-    public Builder createdAtBeforeOrEqual(@NotNull Date createdAtBeforeOrEqual) {
-      this.createdAtBeforeOrEqual = createdAtBeforeOrEqual;
-      return this;
-    }
-
-    @NotNull
-    public Builder createdAtBefore(@NotNull Date createdAtBefore) {
-      this.createdAtBefore = createdAtBefore;
-      return this;
-    }
-
-    @NotNull
-    public MessagePaginationParameters build() {
-      return new MessagePaginationParameters(this);
-    }
-  }
 }
--- src/main/java/io/stream/models/PaginationParameters.java
+++ src/main/java/io/stream/models/PaginationParameters.java
@@ -1,9 +1,10 @@
 package io.stream.models;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
 
+@Builder
 public class PaginationParameters {
   @Nullable
   @JsonProperty("limit")
@@ -28,75 +29,4 @@ public class PaginationParameters {
   @Nullable
   @JsonProperty("id_lt")
   private String idLt;
-
-  private PaginationParameters(Builder builder) {
-    this.limit = builder.limit;
-    this.offset = builder.offset;
-    this.idGte = builder.idGte;
-    this.idGt = builder.idGt;
-    this.idLte = builder.idLte;
-    this.idLt = builder.idLt;
-  }
-
-  /**
-   * Creates builder to build {@link PaginationParameters}.
-   *
-   * @return created builder
-   */
-  public static Builder builder() {
-    return new Builder();
-  }
-
-  /** Builder to build {@link PaginationParameters}. */
-  public static final class Builder {
-    private Integer limit;
-    private Integer offset;
-    private String idGte;
-    private String idGt;
-    private String idLte;
-    private String idLt;
-
-    private Builder() {}
-
-    @NotNull
-    public Builder limit(@NotNull Integer limit) {
-      this.limit = limit;
-      return this;
-    }
-
-    @NotNull
-    public Builder offset(@NotNull Integer offset) {
-      this.offset = offset;
-      return this;
-    }
-
-    @NotNull
-    public Builder idGte(@NotNull String idGte) {
-      this.idGte = idGte;
-      return this;
-    }
-
-    @NotNull
-    public Builder idGt(@NotNull String idGt) {
-      this.idGt = idGt;
-      return this;
-    }
-
-    @NotNull
-    public Builder idLte(@NotNull String idLte) {
-      this.idLte = idLte;
-      return this;
-    }
-
-    @NotNull
-    public Builder idLt(@NotNull String idLt) {
-      this.idLt = idLt;
-      return this;
-    }
-
-    @NotNull
-    public PaginationParameters build() {
-      return new PaginationParameters(this);
-    }
-  }
 }
