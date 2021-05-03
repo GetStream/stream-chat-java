b0da60d639b869e192715302e61627d842e320f2
Cedric Laruelle <cedric@melfatum.com>
2021-05-03 13:16:42 +0200

    refactor: use lombok builder

--- src/main/java/io/stream/models/Sort.java
+++ src/main/java/io/stream/models/Sort.java
@@ -4,7 +4,9 @@ import org.jetbrains.annotations.NotNull;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.Builder;
 
+@Builder
 public class Sort {
   @NotNull
   @JsonProperty("field")
@@ -13,45 +15,6 @@ public class Sort {
   @NotNull
   @JsonProperty("direction")
   private Direction direction;
-
-  private Sort(Builder builder) {
-    this.field = builder.field;
-    this.direction = builder.direction;
-  }
-
-  /**
-   * Creates builder to build {@link Sort}.
-   *
-   * @return created builder
-   */
-  public static Builder builder() {
-    return new Builder();
-  }
-
-  /** Builder to build {@link Sort}. */
-  public static final class Builder {
-    private String field;
-    private Direction direction;
-
-    private Builder() {}
-
-    @NotNull
-    public Builder field(@NotNull String field) {
-      this.field = field;
-      return this;
-    }
-
-    @NotNull
-    public Builder direction(@NotNull Direction direction) {
-      this.direction = direction;
-      return this;
-    }
-
-    @NotNull
-    public Sort build() {
-      return new Sort(this);
-    }
-  }
 
   @JsonFormat(shape = JsonFormat.Shape.NUMBER)
   public enum Direction {
