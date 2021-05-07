32c88298582b8ff3b81da1dba690b390878ac475
Cedric Laruelle <cedric@melfatum.com>
2021-05-07 06:55:51 +0200

    test: increase pause in tests (as still fails sometimes)

--- src/test/java/io/stream/BasicTest.java
+++ src/test/java/io/stream/BasicTest.java
@@ -181,7 +181,7 @@ public class BasicTest {
    */
   protected void pause() {
     try {
-      Thread.sleep(2000);
+      Thread.sleep(3000);
     } catch (InterruptedException e) {
       // Do nothing
     }
