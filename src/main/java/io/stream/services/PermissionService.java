458c05e6df383ec812e389ea08977ca679d92e27
Cedric Laruelle <cedric@melfatum.com>
2021-05-07 05:22:09 +0200

    feat: custom permission endpoints

---
+++ src/main/java/io/stream/services/PermissionService.java
@@ -0,0 +1,40 @@
+package io.stream.services;
+
+import org.jetbrains.annotations.NotNull;
+import io.stream.models.Permission.PermissionCreateRequestData;
+import io.stream.models.Permission.PermissionGetResponse;
+import io.stream.models.Permission.PermissionListResponse;
+import io.stream.models.Permission.PermissionUpdateRequestData;
+import io.stream.models.framework.StreamResponseObject;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+
+public interface PermissionService {
+
+  @POST("custom_permission")
+  @NotNull
+  Call<StreamResponseObject> create(
+      @NotNull @Body PermissionCreateRequestData permissionCreateRequestData);
+
+  @GET("custom_permission/{name}")
+  @NotNull
+  Call<PermissionGetResponse> get(@NotNull @Path("name") String name);
+
+  @POST("custom_permission/{name}")
+  @NotNull
+  Call<StreamResponseObject> update(
+      @NotNull @Path("name") String name,
+      @NotNull @Body PermissionUpdateRequestData permissionUpdateRequestData);
+
+  @DELETE("custom_permission/{name}")
+  @NotNull
+  Call<StreamResponseObject> delete(@NotNull @Path("name") String name);
+
+  @GET("custom_permission")
+  @NotNull
+  Call<PermissionListResponse> list();
+}
---
+++ src/test/java/io/stream/PermissionTest.java
@@ -0,0 +1,76 @@
+package io.stream;
+
+import io.stream.models.Permission;
+import io.stream.models.Permission.Resource;
+import java.util.List;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PermissionTest extends BasicTest {
+
+  @DisplayName("Can create permission")
+  @Test
+  void whenCreatingPermission_thenNoException() {
+    Assertions.assertDoesNotThrow(
+        () ->
+            Permission.create()
+                .name(RandomStringUtils.randomAlphabetic(10))
+                .resource(Resource.ADD_LINKS)
+                .request());
+  }
+
+  @DisplayName("Can update permission")
+  @Test
+  void whenUpdatingPermission_thenNoException() {
+    String name = RandomStringUtils.randomAlphabetic(10);
+    Assertions.assertDoesNotThrow(
+        () -> Permission.create().name(name).resource(Resource.ADD_LINKS).request());
+    pause();
+    Assertions.assertDoesNotThrow(
+        () -> Permission.update(name).resource(Resource.ADD_LINKS).owner(true).request());
+  }
+
+  @DisplayName("Can retrieve permission")
+  @Test
+  void whenRetrievingPermission_thenCorrectName() {
+    String name = RandomStringUtils.randomAlphabetic(10);
+    Assertions.assertDoesNotThrow(
+        () -> Permission.create().name(name).resource(Resource.ADD_LINKS).request());
+    pause();
+    Permission retrievedPermission =
+        Assertions.assertDoesNotThrow(() -> Permission.get(name).request()).getPermission();
+    Assertions.assertEquals(name, retrievedPermission.getName());
+  }
+
+  @DisplayName("Can delete permission")
+  @Test
+  void whenDeletingPermission_thenDeleted() {
+    String name = RandomStringUtils.randomAlphabetic(10);
+    Assertions.assertDoesNotThrow(
+        () -> Permission.create().name(name).resource(Resource.ADD_LINKS).request());
+    pause();
+    Assertions.assertDoesNotThrow(() -> Permission.delete(name).request());
+    pause();
+    List<Permission> permissions =
+        Assertions.assertDoesNotThrow(() -> Permission.list().request()).getPermissions();
+    Assertions.assertFalse(
+        permissions.stream()
+            .anyMatch(consideredPermission -> consideredPermission.getName().equals(name)));
+  }
+
+  @DisplayName("Can list permissions")
+  @Test
+  void whenListingPermission_thenCanRetrieve() {
+    String name = RandomStringUtils.randomAlphabetic(10);
+    Assertions.assertDoesNotThrow(
+        () -> Permission.create().name(name).resource(Resource.ADD_LINKS).request());
+    pause();
+    List<Permission> permissions =
+        Assertions.assertDoesNotThrow(() -> Permission.list().request()).getPermissions();
+    Assertions.assertTrue(
+        permissions.stream()
+            .anyMatch(consideredPermission -> consideredPermission.getName().equals(name)));
+  }
+}
