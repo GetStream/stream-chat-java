9c6319906a2e9f183d98f0b0ec93a43dd5511269
Cedric Laruelle <cedric@melfatum.com>
2021-05-13 07:48:51 +0200

    refactor: filter condition does not contain field name anymore

--- src/main/java/io/stream/models/FilterCondition.java
+++ src/main/java/io/stream/models/FilterCondition.java
@@ -19,61 +19,60 @@ public class FilterCondition {
     return Collections.singletonMap("$nor", Arrays.asList(filter1, filter2));
   }
 
-  public static Map<String, Object> autocomplete(String fieldName, String inputString) {
-    return Collections.singletonMap(
-        fieldName, Collections.singletonMap("$autocomplete", inputString));
+  public static Map<String, Object> autocomplete(String inputString) {
+    return Collections.singletonMap("$autocomplete", inputString);
   }
 
-  public static Map<String, Object> contains(String fieldName, String inputString) {
-    return Collections.singletonMap(fieldName, Collections.singletonMap("$contains", inputString));
+  public static Map<String, Object> contains(String inputString) {
+    return Collections.singletonMap("$contains", inputString);
   }
 
-  public static Map<String, Object> eq(String fieldName, Object fieldValue) {
-    return Collections.singletonMap(fieldName, Collections.singletonMap("$eq", fieldValue));
+  public static Map<String, Object> eq(Object fieldValue) {
+    return Collections.singletonMap("$eq", fieldValue);
   }
 
-  public static Map<String, Object> greaterThan(String fieldName, Object fieldValue) {
-    return Collections.singletonMap(fieldName, Collections.singletonMap("$gt", fieldValue));
+  public static Map<String, Object> greaterThan(Object fieldValue) {
+    return Collections.singletonMap("$gt", fieldValue);
   }
 
-  public static Map<String, Object> greaterThanEquals(String fieldName, Object fieldValue) {
-    return Collections.singletonMap(fieldName, Collections.singletonMap("$gte", fieldValue));
+  public static Map<String, Object> greaterThanEquals(Object fieldValue) {
+    return Collections.singletonMap("$gte", fieldValue);
   }
 
-  public static Map<String, Object> lessThan(String fieldName, Object fieldValue) {
-    return Collections.singletonMap(fieldName, Collections.singletonMap("lt", fieldValue));
+  public static Map<String, Object> lessThan(Object fieldValue) {
+    return Collections.singletonMap("lt", fieldValue);
   }
 
-  public static Map<String, Object> lessThanEquals(String fieldName, Object fieldValue) {
-    return Collections.singletonMap(fieldName, Collections.singletonMap("$lte", fieldValue));
+  public static Map<String, Object> lessThanEquals(Object fieldValue) {
+    return Collections.singletonMap("$lte", fieldValue);
   }
 
-  public static Map<String, Object> ne(String fieldName, Object fieldValue) {
-    return Collections.singletonMap(fieldName, Collections.singletonMap("$ne", fieldValue));
+  public static Map<String, Object> ne(Object fieldValue) {
+    return Collections.singletonMap("$ne", fieldValue);
   }
 
-  public static Map<String, Object> in(String fieldName, Collection<Object> fieldValues) {
-    return Collections.singletonMap(fieldName, Collections.singletonMap("$in", fieldValues));
+  public static Map<String, Object> in(Collection<Object> fieldValues) {
+    return Collections.singletonMap("$in", fieldValues);
   }
 
-  public static Map<String, Object> in(String fieldName, Object... fieldValues) {
-    return in(fieldName, Arrays.asList(fieldValues));
+  public static Map<String, Object> in(Object... fieldValues) {
+    return in(Arrays.asList(fieldValues));
   }
 
-  public static Map<String, Object> nin(String fieldName, Collection<Object> fieldValues) {
-    return Collections.singletonMap(fieldName, Collections.singletonMap("$nin", fieldValues));
+  public static Map<String, Object> nin(Collection<Object> fieldValues) {
+    return Collections.singletonMap("$nin", fieldValues);
   }
 
-  public static Map<String, Object> nin(String fieldName, Object... fieldValues) {
-    return in(fieldName, Arrays.asList(fieldValues));
+  public static Map<String, Object> nin(Object... fieldValues) {
+    return nin(Arrays.asList(fieldValues));
   }
 
-  public static Map<String, Object> exists(String fieldName) {
-    return Collections.singletonMap(fieldName, Collections.singletonMap("$exists", true));
+  public static Map<String, Object> exists() {
+    return Collections.singletonMap("$exists", true);
   }
 
   public static Map<String, Object> notExists(String fieldName) {
-    return Collections.singletonMap(fieldName, Collections.singletonMap("$exists", false));
+    return Collections.singletonMap("$exists", false);
   }
 
   public static Map<String, Object> distinct(Collection<String> memberIds) {
