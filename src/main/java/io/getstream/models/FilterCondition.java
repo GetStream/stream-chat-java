a7b7c1f64816ecdc576f6be40c2e742c02a38892
Cedric Laruelle <cedric@melfatum.com>
2021-05-28 12:07:40 +0200

    refactor: change the way to create filter conditions

--- src/main/java/io/getstream/models/FilterCondition.java
+++ src/main/java/io/getstream/models/FilterCondition.java
@@ -5,84 +5,88 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import org.jetbrains.annotations.NotNull;
 
 public class FilterCondition {
-  public static Map<String, Object> and(Map<String, Object> filter1, Map<String, Object> filter2) {
-    return Collections.singletonMap("$and", Arrays.asList(filter1, filter2));
+  @SafeVarargs
+  public static Map<String, Object> and(@NotNull Map<String, Object>... filters) {
+    return Collections.singletonMap("$and", Arrays.asList(filters));
   }
 
-  public static Map<String, Object> or(Map<String, Object> filter1, Map<String, Object> filter2) {
-    return Collections.singletonMap("$or", Arrays.asList(filter1, filter2));
+  @SafeVarargs
+  public static Map<String, Object> or(@NotNull Map<String, Object>... filters) {
+    return Collections.singletonMap("$or", Arrays.asList(filters));
   }
 
-  public static Map<String, Object> nor(Map<String, Object> filter1, Map<String, Object> filter2) {
-    return Collections.singletonMap("$nor", Arrays.asList(filter1, filter2));
+  @SafeVarargs
+  public static Map<String, Object> nor(@NotNull Map<String, Object>... filters) {
+    return Collections.singletonMap("$nor", Arrays.asList(filters));
   }
 
-  public static Map<String, Object> autocomplete(String inputString) {
-    return Collections.singletonMap("$autocomplete", inputString);
+  public static Map<String, Object> autocomplete(@NotNull String fieldName, @NotNull String inputString) {
+    return Collections.singletonMap(fieldName, Collections.singletonMap("$autocomplete", inputString));
   }
 
-  public static Map<String, Object> contains(String inputString) {
-    return Collections.singletonMap("$contains", inputString);
+  public static Map<String, Object> contains(@NotNull String fieldName, @NotNull String inputString) {
+    return Collections.singletonMap(fieldName, Collections.singletonMap("$contains", inputString));
   }
 
-  public static Map<String, Object> eq(Object fieldValue) {
-    return Collections.singletonMap("$eq", fieldValue);
+  public static Map<String, Object> eq(@NotNull String fieldName, @NotNull Object fieldValue) {
+    return Collections.singletonMap(fieldName, Collections.singletonMap("$eq", fieldValue));
   }
 
-  public static Map<String, Object> greaterThan(Object fieldValue) {
-    return Collections.singletonMap("$gt", fieldValue);
+  public static Map<String, Object> greaterThan(@NotNull String fieldName, @NotNull Object fieldValue) {
+    return Collections.singletonMap(fieldName, Collections.singletonMap("$gt", fieldValue));
   }
 
-  public static Map<String, Object> greaterThanEquals(Object fieldValue) {
-    return Collections.singletonMap("$gte", fieldValue);
+  public static Map<String, Object> greaterThanEquals(@NotNull String fieldName, @NotNull Object fieldValue) {
+    return Collections.singletonMap(fieldName, Collections.singletonMap("$gte", fieldValue));
   }
 
-  public static Map<String, Object> lessThan(Object fieldValue) {
-    return Collections.singletonMap("lt", fieldValue);
+  public static Map<String, Object> lessThan(@NotNull String fieldName, @NotNull Object fieldValue) {
+    return Collections.singletonMap(fieldName, Collections.singletonMap("lt", fieldValue));
   }
 
-  public static Map<String, Object> lessThanEquals(Object fieldValue) {
-    return Collections.singletonMap("$lte", fieldValue);
+  public static Map<String, Object> lessThanEquals(@NotNull String fieldName, @NotNull Object fieldValue) {
+    return Collections.singletonMap(fieldName, Collections.singletonMap("$lte", fieldValue));
   }
 
-  public static Map<String, Object> ne(Object fieldValue) {
-    return Collections.singletonMap("$ne", fieldValue);
+  public static Map<String, Object> ne(@NotNull String fieldName, @NotNull Object fieldValue) {
+    return Collections.singletonMap(fieldName, Collections.singletonMap("$ne", fieldValue));
   }
 
-  public static Map<String, Object> in(Collection<Object> fieldValues) {
-    return Collections.singletonMap("$in", fieldValues);
+  public static Map<String, Object> in(@NotNull String fieldName, @NotNull Collection<Object> fieldValues) {
+    return Collections.singletonMap(fieldName, Collections.singletonMap("$in", fieldValues));
   }
 
-  public static Map<String, Object> in(Object... fieldValues) {
-    return in(Arrays.asList(fieldValues));
+  public static Map<String, Object> in(@NotNull String fieldName, @NotNull Object... fieldValues) {
+    return in(fieldName, Arrays.asList(fieldValues));
   }
 
-  public static Map<String, Object> nin(Collection<Object> fieldValues) {
-    return Collections.singletonMap("$nin", fieldValues);
+  public static Map<String, Object> nin(@NotNull String fieldName, @NotNull Collection<Object> fieldValues) {
+    return Collections.singletonMap(fieldName, Collections.singletonMap("$nin", fieldValues));
   }
 
-  public static Map<String, Object> nin(Object... fieldValues) {
-    return nin(Arrays.asList(fieldValues));
+  public static Map<String, Object> nin(@NotNull String fieldName, @NotNull Object... fieldValues) {
+    return nin(fieldName, Arrays.asList(fieldValues));
   }
 
   public static Map<String, Object> exists() {
     return Collections.singletonMap("$exists", true);
   }
 
-  public static Map<String, Object> notExists(String fieldName) {
-    return Collections.singletonMap("$exists", false);
+  public static Map<String, Object> notExists(@NotNull String fieldName) {
+    return Collections.singletonMap(fieldName, Collections.singletonMap("$exists", false));
   }
 
-  public static Map<String, Object> distinct(Collection<String> memberIds) {
-    Map<String, Object> result = new HashMap<>();
-    result.put("$distinct", true);
-    result.put("$members", memberIds);
-    return result;
+  public static Map<String, Object> distinct(@NotNull String fieldName, @NotNull Collection<String> memberIds) {
+    Map<String, Object> map = new HashMap<>();
+    map.put("$distinct", true);
+    map.put("$members", memberIds);
+    return Collections.singletonMap(fieldName, map);
   }
 
-  public static Map<String, Object> distinct(String... memberIds) {
-    return distinct(memberIds);
+  public static Map<String, Object> distinct(@NotNull String fieldName, @NotNull String... memberIds) {
+    return distinct(fieldName, memberIds);
   }
 }
