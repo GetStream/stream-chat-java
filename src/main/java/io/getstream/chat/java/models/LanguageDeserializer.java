0a707bcec7e7ee0594df14c8be551e0e6d5726a7
ferhat elmas <elmas.ferhat@gmail.com>
2022-11-02 20:37:03 +0100

    fix: skip if unknown

--- src/main/java/io/getstream/chat/java/models/LanguageDeserializer.java
+++ src/main/java/io/getstream/chat/java/models/LanguageDeserializer.java
@@ -16,6 +16,9 @@ public class LanguageDeserializer extends JsonDeserializer<Language> {
       return null;
     }
     for (Language enumValue : Language.values()) {
+      if (enumValue == Language.UNKNOWN) {
+        continue;
+      }
       try {
         if (jsonString.equals(
             Language.class.getField(enumValue.name()).getAnnotation(JsonProperty.class).value())) {
