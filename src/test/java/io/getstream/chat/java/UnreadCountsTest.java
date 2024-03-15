a9dd1724c0e5e5e55d330b86e157fd49b301f494
Ali Momen Sani <ali.momensani91@gmail.com>
2024-03-15 14:50:07 +0100

    fix: create users each time for the tests

--- src/test/java/io/getstream/chat/java/UnreadCountsTest.java
+++ src/test/java/io/getstream/chat/java/UnreadCountsTest.java
@@ -1,6 +1,9 @@
 package io.getstream.chat.java;
 
+import io.getstream.chat.java.exceptions.StreamException;
 import io.getstream.chat.java.models.Channel;
+import io.getstream.chat.java.models.Channel.ChannelMemberRequestObject;
+import io.getstream.chat.java.models.Channel.ChannelRequestObject;
 import io.getstream.chat.java.models.Message;
 import io.getstream.chat.java.models.Message.MessageRequestObject;
 import io.getstream.chat.java.models.UnreadCounts;
@@ -8,24 +11,62 @@ import io.getstream.chat.java.models.UnreadCounts.UnreadCountsBatchResponse;
 import io.getstream.chat.java.models.UnreadCounts.UnreadCountsChannel;
 import io.getstream.chat.java.models.UnreadCounts.UnreadCountsResponse;
 import io.getstream.chat.java.models.UnreadCounts.UnreadCountsThread;
+import io.getstream.chat.java.models.User;
 import io.getstream.chat.java.models.User.UserRequestObject;
+import io.getstream.chat.java.models.User.UserUpsertRequestData.UserUpsertRequest;
 import java.util.*;
+import java.util.stream.Collectors;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class UnreadCountsTest extends BasicTest {
+  protected static List<UserRequestObject> users = new ArrayList<>();
+  protected static Channel channel;
+
+  @BeforeEach
+  void createUsersAndChannel() throws StreamException {
+    // create users
+    users.clear();
+    for (int i = 0; i < 3; i++) {
+      users.add(
+          UserRequestObject.builder()
+              .id(RandomStringUtils.randomAlphabetic(10))
+              .name(String.format("user-%d", i + 1))
+              .build());
+    }
+    UserUpsertRequest usersUpsertRequest = User.upsert();
+    users.forEach(user -> usersUpsertRequest.user(user));
+    usersUpsertRequest.request();
+    // create channel
+    channel =
+        Assertions.assertDoesNotThrow(
+                () ->
+                    Channel.getOrCreate("messaging", RandomStringUtils.randomAlphabetic(12))
+                        .data(
+                            ChannelRequestObject.builder()
+                                .createdBy(users.get(0))
+                                .members(
+                                    users.stream()
+                                        .map(
+                                            user ->
+                                                ChannelMemberRequestObject.builder()
+                                                    .user(user)
+                                                    .build())
+                                        .collect(Collectors.toList()))
+                                .build())
+                        .request())
+            .getChannel();
+  }
+
   @DisplayName("UnreadCounts get does not throw Exception")
   @Test
   void whenCallingGetUnreadCounts_thenNoException() {
-    String firstUserId = testUserRequestObject.getId();
-    String secondUserId = testUsersRequestObjects.get(1).getId();
+    String firstUserId = users.get(0).getId();
+    String secondUserId = users.get(1).getId();
 
-    UnreadCountsResponse r =
-        Assertions.assertDoesNotThrow(() -> UnreadCounts.get(secondUserId).request());
-    var totalAtStart = r.getTotalUnreadCount();
-
-    Channel channel = Assertions.assertDoesNotThrow(() -> createRandomChannel()).getChannel();
     String text = "Hello @" + secondUserId;
 
     MessageRequestObject messageRequest =
@@ -38,8 +79,9 @@ public class UnreadCountsTest extends BasicTest {
                         .request())
             .getMessage();
 
-    r = Assertions.assertDoesNotThrow(() -> UnreadCounts.get(secondUserId).request());
-    Assertions.assertEquals(totalAtStart + 1, r.getTotalUnreadCount());
+    UnreadCountsResponse r =
+        Assertions.assertDoesNotThrow(() -> UnreadCounts.get(secondUserId).request());
+    Assertions.assertEquals(1, r.getTotalUnreadCount());
     List<UnreadCountsChannel> unreadChannels = r.getChannels();
     Assertions.assertNotEquals(0, unreadChannels.size());
 
@@ -87,9 +129,8 @@ public class UnreadCountsTest extends BasicTest {
   @DisplayName("UnreadCounts batch works")
   @Test
   void whenCallingUnreadCountsBatch_thenNoException() {
-    String firstUserId = testUserRequestObject.getId();
+    String firstUserId = users.get(0).getId();
 
-    Channel channel = Assertions.assertDoesNotThrow(() -> createRandomChannel()).getChannel();
     String text = "Hello world!";
 
     MessageRequestObject messageRequest =
@@ -106,18 +147,18 @@ public class UnreadCountsTest extends BasicTest {
         Assertions.assertDoesNotThrow(
             () ->
                 UnreadCounts.batch()
-                    .userIds(testUsersRequestObjects.stream().map(tu -> tu.getId()).toList())
+                    .userIds(users.stream().map(tu -> tu.getId()).collect(Collectors.toList()))
                     .request());
 
     var countsByUser = r.getCountsByUser();
-    for (UserRequestObject u : testUsersRequestObjects) {
+    for (UserRequestObject u : users) {
       var userId = u.getId();
       Assertions.assertTrue(countsByUser.keySet().contains(userId));
       if (userId.equals(firstUserId)) {
         Assertions.assertEquals(0, countsByUser.get(userId).getTotalUnreadCount());
         continue;
       }
-      Assertions.assertEquals(2, countsByUser.get(userId).getTotalUnreadCount());
+      Assertions.assertEquals(1, countsByUser.get(userId).getTotalUnreadCount());
 
       // send this message to add user to the thread
       Assertions.assertDoesNotThrow(
@@ -147,11 +188,11 @@ public class UnreadCountsTest extends BasicTest {
         Assertions.assertDoesNotThrow(
             () ->
                 UnreadCounts.batch()
-                    .userIds(testUsersRequestObjects.stream().map(tu -> tu.getId()).toList())
+                    .userIds(users.stream().map(tu -> tu.getId()).collect(Collectors.toList()))
                     .request());
 
     countsByUser = r.getCountsByUser();
-    for (UserRequestObject u : testUsersRequestObjects) {
+    for (UserRequestObject u : users) {
       var userId = u.getId();
       Assertions.assertTrue(countsByUser.keySet().contains(userId));
       if (userId.equals(firstUserId)) {
